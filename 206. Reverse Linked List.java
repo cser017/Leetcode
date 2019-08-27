66a1860c540af223e28633209414605e27c38815
Chen <excaliburn100@gmail.com>
2019-08-27 19:07:54 +0800

    added recursive

--- 206. Reverse Linked List.java
+++ 206. Reverse Linked List.java
@@ -7,6 +7,7 @@
  * }
  */
 class Solution {
+    // Iterative
     public ListNode reverseList(ListNode head) {
         ListNode pre = null, cur = head;
         while(cur != null){
@@ -16,5 +17,16 @@ class Solution {
             cur = next;
         }
         return pre;
+    }
+    
+    // Recursive
+    public ListNode reverseList2(ListNode head) {    
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode res = reverseList(head.next);  
+        head.next.next = head;
+        head.next = null;
+        return res;
     }
 }
