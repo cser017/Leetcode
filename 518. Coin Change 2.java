44bc44d0c964a0362ba3850f092ed298afab8c65
Chen <excaliburn100@gmail.com>
2019-11-05 14:37:30 -0800

    518. Coin Change 2

---
+++ 518. Coin Change 2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;        
+        for (int num: coins) {
+            for (int i = 1; i <= amount; i++) {
+                if (i - num >= 0) {
+                    dp[i] += dp[i - num];
+                }
+            }
+        }
+        return dp[amount];
+    }
+}
